37630f302ef8200e7b84c03fa587f98019f210ec
madan-stha <madan@scieverinc.com>
2024-09-08 16:58:33 +0545

    Sidebar UI

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,14 +1,114 @@
-import React from "react";
+import { Avatar, Box, Divider, IconButton, Stack } from "@mui/material";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import { Gear } from "phosphor-react";
+import Logo from "../../assets/Images/logo.ico";
+import { Nav_Buttons } from "../../data";
+import { faker } from "@faker-js/faker";
 
 const DashboardLayout = () => {
+  const theme = useTheme();
+  const [selected, setSelected] = useState(0);
+
+  console.log(faker.image.avatar())
 
   return (
     <>
-      Dashboard Layout
+      <Box
+        p={2}
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+          height: "100vh",
+          width: 100,
+        }}
+      >
+        <Stack
+          direction="column"
+          alignItems={"center"}
+          sx={{ width: "100%" }}
+          spacing={3}
+        >
+          <Box
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+            }}
+          >
+            <img src={Logo} alt={"Chat App Logo"} />
+          </Box>
+          <Stack
+            sx={{ width: "max-content" }}
+            direction="column"
+            alignItems="center"
+            spacing={3}
+          >
+            {Nav_Buttons.map((el) =>
+              el.index === selected ? (
+                <Box
+                  sx={{
+                    backgroundColor:
+                      theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton
+                    sx={{
+                      width: "max-content",
+                      color: "#fff",
+                    }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    setSelected(el.index);
+                  }}
+                  sx={{ width: "max-content", color: "#000" }}
+                  key={el.index}
+                >
+                  {el.icon}
+                </IconButton>
+              )
+            )}
+            <Divider sx={{ width: "48px" }} />
+            {selected === 3 ? (
+              <Box
+                sx={{
+                  backgroundColor: theme.palette.primary.main,
+                  borderRadius: 1.5,
+                }}
+              >
+                <IconButton
+                  sx={{ width: "max-content", color: "#fff" }}
+                >
+                  <Gear />
+                </IconButton>
+              </Box>
+            ) : (
+              <IconButton
+                onClick={() => {
+                  setSelected(3);
+                }}
+                sx={{ width: "max-content", color: "#000" }}
+              >
+                <Gear />
+              </IconButton>
+            )}
+          </Stack>
+          <Avatar src={faker.image.avatar()} />
+        </Stack>
+      </Box>
       <Outlet />
     </>
   );
 };
 
 export default DashboardLayout;
+
--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,14 @@
-import React from "react";
-
+import React, { Suspense, lazy } from "react";
+const Cat = lazy(() => import("../../components/Cat"))
 const GeneralApp = () => {
 
-  return (
-    <>
-      App
-    </>
-  );
+    return (
+        <>
+            <Suspense fallback="Loading...">
+                <Cat />
+            </Suspense>
+        </>
+    );
 };
 
 export default GeneralApp;
